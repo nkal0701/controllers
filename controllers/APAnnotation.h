d1e8e44eb17ac91d07089a6ea44b1856082299f1
Alfred Marius Nkemla Lienou <nkemlamarius@gmail.com>
2014-01-26 06:26:32 -0600

    AP opens map with a centered location

---
+++ controllers/APAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  APAnnotation.h
+//  controllers
+//
+//  Created by Alfred Marius Nkemla Lienou on 1/26/14.
+//  Copyright (c) 2014 Alfred Marius Nkemla Lienou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APAnnotation : NSObject
+
+@end
---
+++ controllers/APAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  APAnnotation.m
+//  controllers
+//
+//  Created by Alfred Marius Nkemla Lienou on 1/26/14.
+//  Copyright (c) 2014 Alfred Marius Nkemla Lienou. All rights reserved.
+//
+
+#import "APAnnotation.h"
+
+@implementation APAnnotation
+
+@end
